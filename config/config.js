8b9670a85a845c8c85dfdbe94febb61a7df1e2e3
Sdraje <alfmarmil@gmail.com>
2016-09-16 11:22:30 +0100

    Added engine to json

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:   process.env.PORT || 3000,
-  db:     process.env.MONGOLAB_URI || "mongodb://localhost/eventi",
+  db:     process.env.MONGODB_URI || "mongodb://localhost/eventi",
   secret: process.env.SECRET || "SUPAH SECRET"
 };
